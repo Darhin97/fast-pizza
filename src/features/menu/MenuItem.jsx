34465f5df6fe36f5ac1f285beabbad21710f7a6b
Derrick Ankrah <darhin97@gmail.com>
2023-07-26 18:26:04 +0000

    ADD: delete and update feat

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,17 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utilities/helpers';
-import { addItem } from '../cart/cartSlice';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+
+  const IsInCart = currentQuantity > 0;
 
   const handleAddToCart = () => {
     const newItem = {
@@ -41,7 +47,17 @@ function MenuItem({ pizza }) {
             <p>Sold out</p>
           )}
 
-          {!soldOut && (
+          {IsInCart && (
+            <div className=" flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
+
+          {!soldOut && !IsInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
